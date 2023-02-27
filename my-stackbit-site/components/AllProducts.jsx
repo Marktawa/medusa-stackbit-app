2643527ff3f7d23a6045750aac696d96e619a6b9
Mark Munyaka <markmunyaka@gmail.com>
2023-02-27 12:00:59 +0000

    Fetch products from Medusa Server

--- my-stackbit-site/components/AllProducts.jsx
+++ my-stackbit-site/components/AllProducts.jsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
+import { useState, useEffect } from "react";
 
 export const AllProducts = (props) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('http://localhost:9000/store/products');
+      const data = await response.json();
+      setProducts(data.products);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>{props.heading}</h1>
       <ul>
-        {props.products.map((product) => (
+        {products.map((product) => (
           <li key={product.id}>
-            <h2>{product.name}</h2>
+            <h2>{product.title}</h2>
             <p>{product.description}</p>
-            <p>Price: {product.price}</p>
+            <p>Price: ${product.variants[0].prices[0].amount}</p>
+            <p>{product.handle}</p>
           </li>
         ))}
       </ul>
