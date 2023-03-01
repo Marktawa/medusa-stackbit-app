50076fcdcd9132144725c53a0e30f5d3d9f1f663
Mark Munyaka <markmunyaka@gmail.com>
2023-03-01 16:13:30 +0000

    Added inline editing to header

--- my-stackbit-site/components/Header.jsx
+++ my-stackbit-site/components/Header.jsx
@@ -1,11 +1,12 @@
 export const Header = ({ siteConfig }) => {
+    const headerObjectId = siteConfig.__id + ':header';
     return (
-        <header className="header outer">
+        <header className="header outer" data-sb-field-path={headerObjectId}>
             <div className="inner">
-                <span className="header-title">{siteConfig.header.title}</span>
-                <span className="header-nav">
-                    {siteConfig.header.navLinks.map((navLink) => (
-                        <a href={`${navLink.url}`}>{navLink.label}</a>
+                <span className="header-title" data-sb-field-path=".title">{siteConfig.header.title}</span>
+                <span className="header-nav" data-sb-field-path=".navLinks">
+                    {siteConfig.header.navLinks.map((navLink, idx) => (
+                        <a href={`${navLink.url}`} data-sb-field-path={`.${idx}`}>{navLink.label}</a>
                     ))}
                 </span>
             </div>
